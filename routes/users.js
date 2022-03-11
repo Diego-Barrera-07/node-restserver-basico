a1c946df54d50b9a66823a56ce2af9e5300ad387
DiegoBarrera <7diegofernando@gmail.com>
2022-03-10 22:20:20 -0500

    tryyyyyyyy conexioooooooon db

--- routes/users.js
+++ routes/users.js
@@ -5,25 +5,25 @@ const { usuariosGet, usuariosPost, usuariosDelete, usuariosPut, usuariosPatch } 
 const { roleValidation, emailValidation, idValidation } = require('../helpers/db-validators')
 const router = Router()
 
-router.get('/', usuariosGet)
+router.get('/', usuariosGet);
 router.put('/:id', [
     check('id', 'ID no es valido por MONGODB').isMongoId(),
     check('id').custom(idValidation),
     check('role').custom(roleValidation),
     validarCampos
-], usuariosPut)
+], usuariosPut);
 router.post('/', [
     check('name', '---- El nombre es obligatorio -----').not().isEmpty(),
     check('email').custom(emailValidation),
     check('password', '---- La contraseña no es válida, debe al menos ser de seis letras -----').isLength({ min: 6 }),
     check('role').custom(roleValidation),
     validarCampos
-], usuariosPost)
+], usuariosPost);
 router.delete('/:id', [
     check('id', 'ID no es valido por MONGODB').isMongoId(),
     check('id').custom(idValidation),
     validarCampos
-], usuariosDelete)
+], usuariosDelete);
 router.patch('/', usuariosPatch)
 
 module.exports = router
