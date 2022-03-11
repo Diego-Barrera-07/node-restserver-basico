2b6411f73b4cc212dfe3294859f4c650d0ca5a3b
DiegoBarrera <7diegofernando@gmail.com>
2022-03-10 21:50:43 -0500

    env borrado

--- database/config.js
+++ database/config.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
 const dbConnection = () => {
-    const tryToConnect = 'mongodb+srv://diegobarrera07:4bx1TrEXVYUpi0s4@cafenodedb.w0ome.mongodb.net/cafeDB'
     try {
-        mongoose.connect(process.env.MONGODB_CONNECTION || tryToConnect, {
+        mongoose.connect(process.env.MONGODB_CONNECTION, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
