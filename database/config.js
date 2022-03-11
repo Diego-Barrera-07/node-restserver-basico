82ac8ca15171c72d33c2915488093386ea067f71
DiegoBarrera <7diegofernando@gmail.com>
2022-03-10 21:43:14 -0500

    tryyyyyyyy conexioooooooon db

--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbConnection = async () => {
+const dbConnection = () => {
     const tryToConnect = 'mongodb+srv://diegobarrera07:4bx1TrEXVYUpi0s4@cafenodedb.w0ome.mongodb.net/cafeDB'
     try {
         mongoose.connect(process.env.MONGODB_CONNECTION || tryToConnect, {
